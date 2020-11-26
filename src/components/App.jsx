3995ea4060c7247d38b384c1f9a75388ed97c72a
Tim <tim.langner@tobit.com>
2020-11-26 15:15:03 +0100

    :sparkles: Render bubbles with colors from color list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Bubble from "./bubble/Bubble";
+import ColorPicker from "./colorPicker/ColorPicker";
+import { hexToRgb } from "../utils/hexToRgb";
+import { relativeLuminanceW3C } from "../utils/relativeLuminance";
+import './app.scss';
 
-function App() {
+const App = () => {
+    const [backgroundColor, setBackgroundColor] = useState("#476D91");
+    const [data, setData] = useState(null);
+    const [contentHeight, setContentHeight] = useState(0);
+
+    const colorList = [
+        "#666666",
+        "#707FAF",
+        "#37913C",
+        "#AA0000",
+        "#5E4883",
+        "#FF9800",
+        "#0055A4",
+        "#9F713A",
+        "#828282",
+        "#A83D2E",
+        "#486D83",
+    ];
+
+    useEffect(() => {
+        setContentHeight(document.querySelector('.tapp').clientHeight);
+        chayns.getWindowMetrics().then((windowData) => {
+            console.log(windowData.height);
+            console.log(document.querySelector('.tapp').clientHeight)
+            setData(windowData);
+        });
+    }, []);
 
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div style={{ padding: '16px', backgroundColor: backgroundColor }}>
+            <ColorPicker color={backgroundColor} setColor={setBackgroundColor} />
+            {
+                colorList.map((color) => {
+                    return (
+                        <Bubble backgroundColor={backgroundColor} textColor={color} key={color} />
+                    );
+                })
+            }
         </div>
     );
 }
