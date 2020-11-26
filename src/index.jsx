804b3e05118d6e97f45d1789348700c98d0281a8
Tim <tim.langner@tobit.com>
2020-11-26 10:44:07 +0100

    :fire: Delete useless code

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-import Bowser from 'bowser';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -6,13 +5,6 @@ import App from './components/App';
 async function init() {
     try {
         await chayns.ready;
-
-        const parser = Bowser.getParser(window.navigator.userAgent);
-        const browser = {
-            browser: parser.getBrowser(),
-            os: parser.getOS(),
-            platform: parser.getPlatform(),
-        };
 
         ReactDOM.render(
             <App />,
