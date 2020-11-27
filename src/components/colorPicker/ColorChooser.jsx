271b8d1f4c51e9c595e4f927d641d98a20c72eec
Tim <tim.langner@tobit.com>
2020-11-27 15:47:12 +0100

    :lipstick: Add chrome color picker

--- src/components/colorPicker/ColorChooser.jsx
+++ src/components/colorPicker/ColorChooser.jsx
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from "react";
-import { ColorPicker } from "chayns-components";
+import React from "react";
 import { ChromePicker } from 'react-color';
 let convert = require('color-convert');
 import './colorPicker.scss';
@@ -8,8 +7,6 @@ const ColorChooser = ({ color, setColor }) => {
 
     const handleColorChange = (color) => {
         setColor(color.hex);
-        // console.log(`#${convert.hsv.hex(`${data.h.toFixed(0)}, ${data.s.toFixed(0)}, ${data.v.toFixed(0)}`)}`);
-        // setColor(`#${convert.hsv.hex(`${data.h.toFixed(0)}, ${data.s.toFixed(0)}, ${data.v.toFixed(0)}`)}`);
     };
 
     return (
@@ -20,13 +17,6 @@ const ColorChooser = ({ color, setColor }) => {
                     handleColorChange(color)
                 }}
             />
-
-            {/*<ColorPicker*/}
-            {/*    color={"#476D91"}*/}
-            {/*    onChange={(data) => {*/}
-            {/*        handleColorChange(data)*/}
-            {/*    }}*/}
-            {/*/>*/}
         </div>
     );
 };
